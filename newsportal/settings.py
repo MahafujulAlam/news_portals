d8f86e6565008a7342876e50bacc256aae106e87
Mahafujul Alam Ansari <amahafujul44@gmail.com>
2020-10-19 15:44:15 +0530

    fdf

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+# new
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,16 +89,24 @@ WSGI_APPLICATION = 'newsportal.wsgi.application'
 # }
 
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'newsportal',
+#        'USER': 'postgres',
+#        'PASSWORD': '62628206',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'newsportal',
-       'USER': 'postgres',
-       'PASSWORD': '62628206',
-       'HOST': 'localhost',
-       'PORT': '5432',
-    }
+
+    'default': dj_database_url.config()
 }
+
+
 
 
 
@@ -137,3 +148,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
